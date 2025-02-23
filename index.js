ce4753648711a87f26748c7b9478624b8f8de0e1
oraolt21 <oraolt9@gmail.com>
2025-02-23 20:07:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["/zima", "/zima"];
+const statusMessages = ["Silver JB", "🎮 IP : 51.195.81.38:27021"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
